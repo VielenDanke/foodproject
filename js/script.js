75c31161c43a5021054f7ee59b29c198c7fed936
Vladislav Dankevich <vielendanke1991@gmail.com>
2020-10-09 11:01:18 +0600

    calculator modified

--- js/script.js
+++ js/script.js
@@ -424,4 +424,119 @@ window.addEventListener('DOMContentLoaded', function() {
             dots[slideIndex-1].style.opacity = 1;
         });
     });
+
+    // Calculator
+
+    const result = document.querySelector(".calculating__result span");
+    let sex, 
+        height, 
+        weight, 
+        age, 
+        ratio;
+
+    if (localStorage.getItem("sex")) {
+        sex = localStorage.getItem("sex");
+    } else {
+        sex = "female";
+        localStorage.setItem("sex", sex);
+    }
+
+    if (localStorage.getItem("ratio")) {
+        ratio = localStorage.getItem("ratio");
+    } else {
+        ratio = 1.375;
+        localStorage.setItem("ratio", ratio);
+    }
+
+    function initLocalSettings(selector, activeClass) {
+        const elements = document.querySelectorAll(selector);
+
+        elements.forEach(elem => {
+            elem.classList.remove(activeClass);
+
+            if (elem.getAttribute("id") === localStorage.getItem("sex")) {
+                elem.classList.add(activeClass);
+            }
+            if (elem.getAttribute("data-ratio") === localStorage.getItem("ratio")) {
+                elem.classList.add(activeClass);
+            }
+        });
+    }
+
+    initLocalSettings("#gender div", "calculating__choose-item_active");
+    initLocalSettings(".calculating__choose_big div", "calculating__choose-item_active");
+
+    function calcTotal() {
+        if (!height || !weight || !age) {
+            result.textContent = "____";
+            return;
+        }
+        if (height === "" || weight === "" || age === "") {
+            result.textContent = "____";
+            return;
+        }
+        if (sex === "female") {
+            result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);
+        }
+        if (sex === "male") {
+            result.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio);
+        }
+    }
+
+    function getStaticInformation(selector, activeClass) {
+        const elements = document.querySelectorAll(selector);
+
+        elements.forEach(elem => {
+            elem.addEventListener("click", (event) => {
+                if (event.target.getAttribute("data-ratio")) {
+                    ratio = +event.target.getAttribute("data-ratio");
+                    localStorage.setItem("ratio", ratio);
+                } else {
+                    sex = event.target.getAttribute("id");
+                    localStorage.setItem("sex", sex);
+                }
+                elements.forEach(elem => {
+                    elem.classList.remove(activeClass);
+                });
+    
+                event.target.classList.add(activeClass);
+                
+                calcTotal();
+            });
+        });
+    }
+
+    getStaticInformation("#gender div", "calculating__choose-item_active");
+    getStaticInformation(".calculating__choose_big div", "calculating__choose-item_active");
+
+    function getDynamicInformation(selector) {
+        const input = document.querySelector(selector);
+        
+        input.addEventListener("input", () => {
+            if (input.value.match(/\D/g)) {
+                input.style.border = "1px solid red";
+            } else {
+                input.style.border = "none";
+            }
+
+            const inputId = input.getAttribute("id");
+
+            switch (inputId) {
+                case "weight":
+                    weight = +input.value;
+                    break;
+                case "height":
+                    height = +input.value;
+                    break;
+                case "age":
+                    age = +input.value;
+                    break;        
+            }
+            calcTotal();
+        });
+    }
+
+    getDynamicInformation("#height");
+    getDynamicInformation("#weight");
+    getDynamicInformation("#age");
 });
