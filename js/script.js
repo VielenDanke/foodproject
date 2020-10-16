a1f0b5073184d17ce0442629646b2fb50ab4e45a
Vladislav Dankevich <vielendanke1991@gmail.com>
2020-10-16 17:37:49 +0600

    babel + polyfills

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 "use strict";
 
+require("es6-promise").polyfill();
+
+import 'nodelist-foreach-polyfill';
+
 import tabs from "./modules/tabs";
 import calc from "./modules/calc";
 import cards from "./modules/cards";
